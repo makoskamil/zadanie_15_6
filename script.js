f8229ae6848a606d369d0c35650d0ae33ce244c7
Kamil Makoś <makos.kamil@o2.pl>
2018-06-20 15:07:17 +0200

    add_start_and_stop

--- script.js
+++ script.js
@@ -30,8 +30,37 @@ class Stopwatch {
         return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
     }
 
-}
 
+    start() {
+        if (!this.running) {
+            this.running = true;
+            this.watch = setInterval(() => this.step(), 10);
+        }
+    }
+    
+    step() {
+        if (!this.running) return;
+        this.calculate();
+        this.print();
+    }
+    
+    calculate() {
+        this.times.miliseconds += 1;
+        if (this.times.miliseconds >= 100) {
+            this.times.seconds += 1;
+            this.times.miliseconds = 0;
+        }
+        if (this.times.seconds >= 60) {
+            this.times.minutes += 1;
+            this.times.seconds = 0;
+        }
+    }
+    
+    stop() {
+        this.running = false;
+        clearInterval(this.watch);
+    }
+}
 
 const stopwatch = new Stopwatch(
 document.querySelector('.stopwatch'));
